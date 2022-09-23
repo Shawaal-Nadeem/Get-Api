544cbe3fe0a28199eaa74f378c7bec5f20487e7e
Shawaal-Nadeem <hafizshawalnadeem@gmail.com>
2022-09-23 09:24:20 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  
+const [repos,setRepos]=useState([{}])
+useEffect(()=>{
+  async function getApi(){
+  const api=await fetch('https://api.github.com/users/Shawaal-Nadeem/repos')
+  const json=await api.json()
+  console.log(json)  
+  setRepos(json);
+  }
+  getApi()
+},[]) 
+return (
+  <div>
+    <ul>
+      <b>
+        {repos.map((repoObj, ind) => {
+        return (<li key={ind}>{repoObj.name}</li>)
+        
+
+      })}
+      </b>
+
+    </ul>
+  </div>
+);
 }
+
 
 export default App;
